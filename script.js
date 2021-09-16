8c0949059ffddcb4a32d9e416c489731cb222a54
Glitch (glitch-hello-website) <none>
2021-09-16 04:28:23 +0000

    🖖🏦 Checkpoint ./script.js:291626/30

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ document.getElementById("textTheyTyped").value = "Paste in some text here!";
 document.getElementById("submit").onclick = e => {
   console.log("I got clicked");
   var x = document.getElementById("textTheyTyped").value;
-  var n = / |[a-zA-Z]|[\r\n][0-9]$/g;
+  var n = / |[a-zA-Z]|[\r\n][0-9]/g;
   var almostFinal = x.replace(n,"");
-  var k = /[\r\n]$/g;
+  var k = /[\r\n]/g;
   var final = almostFinal.replace(k,"");
   document.getElementById("textTheyTyped").value = final;
+  console.log(final);
 };
