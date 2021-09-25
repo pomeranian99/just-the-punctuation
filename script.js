8b6be0bdf2bec1163466511563958695a2ba586f
Glitch (glitch-hello-website) <none>
2021-09-25 22:10:50 +0000

    🏠🌓 Checkpoint ./script.js:291626/666

--- script.js
+++ script.js
@@ -9,9 +9,17 @@ document.getElementById("submit").onclick = e => {
   // for some reason I seem to need to run this substitution again, I have no idea why lol
   var k = /[\r\n]|[0-9]/g;
   var final = almostFinal.replace(k,"");
-  // the "final" string winds up having some mystery line-breaks thrown in. Can't figure out how to get 'em out! So, here's my super-hacky way to get rid of them: i) put the charcode of each "final" string as an element into and 
+  // the "final" string winds up having some mystery line-breaks thrown in. Can't figure out how to get 'em out! So, here's my super-hacky way to get rid of them: i) push the charcode of each character in "final" into an array, ii) iterate through that array and add each charcode to a new string
+  var charArray = [];
+  for (let c = 0; c < final.length; c++) {
+    charArray.push(final.charCodeAt(c));
+  }
+  console.log("The array I got is ... ")
+  console.log(charArray);
+  var result = "";
   
-  document.getElementById("textTheyTyped").value = final;
+  
+  document.getElementById("textTheyTyped").value = "Working on it!";
   
 };
 
