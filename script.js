d907b2841dbc36de9a4bbbf48c23e74c4dda9cf4
Glitch (glitch-hello-website) <none>
2021-10-01 12:31:31 +0000

    🏵🏇 Checkpoint ./script.js:291626/648

--- script.js
+++ script.js
@@ -13,8 +13,15 @@ document.getElementById("submit").onclick = e => {
   // the "final" string winds up having some mystery line-breaks thrown in. Can't figure out how to get 'em out! So, here's my super-hacky way to get rid of them: i) push the charcode of each character in "final" into an array, ii) iterate through that array and add each charcode to a new string
   var charArray = [];
   for (let c = 0; c < final.length; c++) {
+    // CODE TO ADD HERE!!
+    // 1) if the character code is less than 128 (and is thus part of the ASCII character set) ...
+    // 2) push
     charArray.push(final.charCodeAt(c));
   }
+  // NOW, what I need to do is ...
+  // 1) iterate through the array, and ...
+  // 2) if the number is below 128 (and thus part of the ASCII character set) ...
+  // 3) turn it into the relevant ascii 
   console.log("The array I got is ... ")
   console.log(charArray);
   var result = "";
@@ -29,6 +36,9 @@ function uniToHTML (y) {
   switch (y) {
     case 8220: 
       x = "&#8220";
+      break;
+    case 8221:
+      x = "&#8221;";
       break;
     case 8217:
       x = "&#8217;"
@@ -51,6 +61,10 @@ function uniToHTML (y) {
     case 59:
       x = "&#59;";
       break;
+    case 39:
+      x = "&#8217;";
+      break;
+      
       
       
   }
