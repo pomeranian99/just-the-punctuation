52e07ade8be28ba271330780f36a3909300a9be2
Glitch (glitch-hello-website) <none>
2021-10-07 02:18:42 +0000

    🚗👑 Checkpoint ./script.js:291626/748

--- script.js
+++ script.js
@@ -10,7 +10,27 @@ var uniToAscii = [
   [8211, 150],  // n-dash
   [8212, 151], // m-dash
   [8212, 151], // m-dash
-  [8126, 39], 
+  [8216, 39], // single quote
+  [8217, 39], // single quote
+  [8219, 39], // single quote
+  [8220, 34], // double quotes
+  [8221, 34], // double quotes
+  [8224, 34], // double quotes
+  [8228, 46], // period
+  [8230, 133], // ellipses
+  [8242, 39], // single quotes
+  [8245, 39], // single quotes
+  [8243, 34], // double quotes
+  [8246, 34], // double quotes
+  [8248, 94], // caret
+  [8249, 139], // single left-pointing angle quotation 
+  [8250, 155], // single right-pointing angle quotation
+  [8259, 45], // hyphen
+  [8260, 47], // forward slash
+  [8267, 182] // pilcrow, paragraph sign
+  [8270, 42], // asterisk
+  
+  
 ]
 
 document.getElementById("submit").onclick = e => {
